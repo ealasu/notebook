d972c0c4e335773b8c456b3b8c5df91359308097
ealasu <emanuel.alasu@gmail.com>
2016-09-21 16:09:31 +0000

    init interface

---
+++ src/lib.rs
@@ -0,0 +1,8 @@
+pub mod notebook;
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+    }
+}
---
+++ src/notebook.rs
@@ -0,0 +1,29 @@
+
+struct Notebook {
+}
+
+impl Notebook {
+    /// make sure `path` is a git repo
+    fn open(path: &Path) -> Self {}
+
+    /// git init
+    fn create(path: &Path) -> Self {}
+
+    /// ls directory
+    fn ls(&self, path: &Path) -> Vec<String> {}
+
+    /// read file
+    fn read(&self, path: &Path) -> String {}
+
+    /// write file, git commit
+    fn write(&self, path: &Path, text: &str) {}
+
+    /// git pull, merge (TODO: handle conflicts?), git push
+    fn synchronize(&self) {}
+
+    /// determine incoming & outgoing changes
+    fn sync_status(&self) -> SyncStatus {}
+
+    /// git blame
+    fn history(&self, path: &Path) -> History {}
+}
